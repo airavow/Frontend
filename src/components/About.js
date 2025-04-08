3b285603e12c5461eecfdb03969d8952ff1a001f
Airavow <airavow@gmail.com>
2025-04-08 16:50:46 +0530

    initial commit

---
+++ src/components/About.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div className='mt-5'>About</div>
+  )
+}
---
+++ src/components/Main.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter, Route, NavLink, Routes } from "react-router-dom";
+// import { Sidebar } from '../ui_components/Sidebar'
+import { Header } from "../ui_components/Header";
+import { Footer } from "../ui_components/Footer";
+import { Home } from "./Home";
+import { About } from "./About";
+import { Blog } from "./Blog";
+
+export const Main = () => {
+  return (
+    <>
+      <div className="container-fluid">
+        <Header />
+        <div className="row">
+          <div className="col-md-12">
+            <BrowserRouter>
+              <Routes>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/home" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/blog" component={Blog} />
+              </Routes>
+            </BrowserRouter>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
+};
